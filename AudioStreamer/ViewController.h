cfe024e6893d392ca77d2e0bd52b10c127a0f2bf
讀帛ｳｶ蜆ｪ <icloud_i4s@me.com>
2015-10-22 16:59:53 +0900

    再生できるとこまで

--- AudioStreamer/ViewController.h
+++ AudioStreamer/ViewController.h
@@ -7,8 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@interface ViewController : UIViewController
+#import <Foundation/Foundation.h>
+#import <MediaPlayer/MediaPlayer.h>
+#import <AVFoundation/AVFoundation.h>
+//#import <AudioToolbox/AudioToolbox.h>
+//#import <AudioUnit/AudioUnit.h>
+#import "myaudioqueue.h"
+@interface ViewController : UIViewController<MPMediaPickerControllerDelegate>
 
 
 @end
--- AudioStreamer/ViewController.m
+++ AudioStreamer/ViewController.m
@@ -9,19 +9,84 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property MPMusicPlayerController * controler;
+@property myaudioqueue * queue;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.queue=[[myaudioqueue alloc]init];
+    
+  
+    
+        // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)SelectBtnTap:(id)sender {
+    
+    MPMediaPickerController *picker = [[MPMediaPickerController alloc]init];
+    picker.delegate = self;
+    picker.allowsPickingMultipleItems = NO;        // 複数選択可
+    [self presentViewController:picker animated:YES completion:nil];
+}
 
+//delegate
+-(void)mediaPicker:(MPMediaPickerController *)mediaPicker didPickMediaItems:(MPMediaItemCollection *)mediaItemCollection
+{
+    MPMediaItem *item=[mediaItemCollection.items objectAtIndex:0];
+    NSURL *url=[item valueForProperty:MPMediaItemPropertyAssetURL];
+    AVURLAsset *urlAsset = [AVURLAsset URLAssetWithURL:url options:nil];
+    AVAssetExportSession *exportSession = [[AVAssetExportSession alloc]
+                                           initWithAsset:urlAsset
+                                           presetName:AVAssetExportPresetAppleM4A];
+    
+    exportSession.outputFileType = [[exportSession supportedFileTypes] objectAtIndex:0];
+    
+    NSString *docDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString *filePath = [[docDir stringByAppendingPathComponent:[item valueForProperty:MPMediaItemPropertyTitle]] stringByAppendingPathExtension:@"aif"];
+    
+    exportSession.outputURL = [NSURL fileURLWithPath:filePath];
+    
+    [exportSession setTimeRange:CMTimeRangeMake(kCMTimeZero, [urlAsset duration])];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    // ファイルを移動
+     [fileManager removeItemAtPath:filePath error:nil];
+    
+
+    // ディレクトリを作成
+    [fileManager createDirectoryAtPath:docDir
+           withIntermediateDirectories:YES
+                            attributes:nil
+                                 error:nil];
+    
+    
+    [exportSession exportAsynchronouslyWithCompletionHandler:^{
+        
+        if (exportSession.status == AVAssetExportSessionStatusCompleted) {
+            NSLog(@"export session completed");
+           self.queue= [self.queue initWithFilepath:exportSession.outputURL];
+            [self.queue play];
+           
+        } else {
+            NSLog(@"export session error");
+            
+        }
+        
+    }];
+    
+    
+    
+    [mediaPicker dismissViewControllerAnimated:YES completion:nil];
+
+}
+-(void)mediaPickerDidCancel:(MPMediaPickerController *)mediaPicker{
+    [mediaPicker dismissViewControllerAnimated:YES completion:nil];
+     }
 @end
