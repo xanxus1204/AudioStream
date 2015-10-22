67c07559e07ee07418325ce9411cfd60e95beee7
讀帛ｳｶ蜆ｪ <icloud_i4s@me.com>
2015-10-22 15:38:16 +0900

    Initial Commit

---
+++ AudioStreamer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioStreamer
+//
+//  Created by 椛島優 on 2015/10/22.
+//  Copyright © 2015年 椛島優. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioStreamer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioStreamer
+//
+//  Created by 椛島優 on 2015/10/22.
+//  Copyright © 2015年 椛島優. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
